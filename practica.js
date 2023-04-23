9eec9d14520031c336a83e3f08b59738d9c3bcb9
Toni Soler <tonisoler1986@gmail.com>
2023-04-23 23:09:22 +0200

    not finished yet

---
+++ practica.js
@@ -0,0 +1,15 @@
+let book3 = {
+    author: "Charlotte Bronte",
+    editions : "seven"
+}
+
+let book4 = {
+    author: "Juan",
+    editions: "seven"
+}
+
+book3 = book4
+
+console.log(book3)
+
+console.log(book4)
